33a5395abf3e4f6c90f881e582c2fb613992192f
ahmed <newl4533@gmail.com>
2025-01-12 14:39:45 +0200

    updating permission

--- company/views.py
+++ company/views.py
@@ -20,19 +20,36 @@ class CompanyViewSet(viewsets.ModelViewSet):
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
-    permission_classes = [IsManager]  # Only Manager can access departments related to their company
+    permission_classes = [IsManager ,IsAdmin]
+
+    def get_queryset(self):
+   
+        if self.request.user.role == 'Manager':
+            return Department.objects.filter(manager=self.request.user)  
+        
+        return super().get_queryset()
+    
+      # Only Manager can access departments related to their company
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
-    permission_classes = [IsEmployee]  # Employees can access only their own data
+    permission_classes = [IsEmployee ,IsAdmin ,IsManager]
+    
+    def get_queryset(self):
+       
+        if self.request.user.role == 'Employee':
+            return Employee.objects.filter(id=self.request.user.id)  
+        
+        return super().get_queryset()  
+    
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = [IsManager]  # Managers can access projects in their department
+    permission_classes = [IsManager,IsAdmin]  # Managers can access projects in their department
 
 class PerformanceReviewViewSet(viewsets.ModelViewSet):
     queryset = PerformanceReview.objects.all()
     serializer_class = PerformanceReviewSerializer
-    permission_classes = [IsAdmin]  # Only Admin can manage performance reviews
+    permission_classes = [IsAdmin ]  # Only Admin can manage performance reviews
